e0cd67cd6b892dd1febd1a007e071d7e1f190633
koder <vineet15.yadav@si-t106.LGE.NET>
2017-03-24 11:55:07 +0530

    Segregate odd and Even Numbers in the array

---
+++ CodingProblems/src/array/SegregateOddEven.java
@@ -0,0 +1,25 @@
+package array;
+
+//Segregate odd and Even Numbers in the array
+
+public class SegregateOddEven {
+	
+	static void segregateOddEven1(int A[], int len){
+		int left = 0;
+		int right = len-1;
+		int temp;
+		
+		while(left < right){
+			
+		}
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
