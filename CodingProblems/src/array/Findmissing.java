643f5d863f2e9fd28203cf10bffc3dd653cf3c86
koder <vineet15.yadav@si-t106.LGE.NET>
2017-03-24 16:30:34 +0530

    find missing element in duplicated array

--- CodingProblems/src/array/Findmissing.java
+++ CodingProblems/src/array/Findmissing.java
@@ -5,17 +5,25 @@ package array;
 public class Findmissing {
 
 	static int findMissingElement(int A[],int B[]){
-		if((A.length != B.length-1) || (B.length != A.length-1)){
+		if((A.length != B.length-1) && (B.length != A.length-1)){
 			return -1;
 		}
 		
-		return 1;
+		int ele = 0;
+		for(int i=0; i < A.length; i++){
+			ele = ele^A[i];
+		}
+		
+		for(int i=0; i < B.length; i++){
+			ele = ele^B[i];
+		}
+		return ele;
 	}
 	
 	public static void main(String[] args) {
 		
 		int arr1[] = new int[]{1,2,3,4,5};
-		int arr2[] = new int[]{2,3,4};
+		int arr2[] = new int[]{1,2,3,4};
 		
 		System.out.print(findMissingElement(arr1, arr2));
 	}
